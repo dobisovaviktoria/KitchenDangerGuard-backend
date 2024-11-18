1188caf288776a5af8476709dacfc428e1d86a18
Viktoria Dobisova <viktoria.dobisova@student.kdg.be>
2024-11-18 13:38:34 +0100

    temporary changes until arduino works

--- src/main/java/projectKDG/service/TemperatureService.java
+++ src/main/java/projectKDG/service/TemperatureService.java
@@ -1,31 +1,31 @@
-package projectKDG.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import projectKDG.domain.Temperature;
-import projectKDG.repository.TemperatureRepository;
-
-import java.util.List;
-
-@Service
-public class TemperatureService{
-    private final TemperatureRepository temperatureRepository;
-
-    @Autowired
-    public TemperatureService(TemperatureRepository temperatureRepository) {
-        this.temperatureRepository = temperatureRepository;
-    }
-
-    @GetMapping
-    public List<Temperature> getTemperatures() {
-        return temperatureRepository.findAll();
-    }
-
-    public void addNewTemperature(Temperature temperature) {
-        temperatureRepository.findTemperatureByTemperatureSensorId(temperature.getTemperatureSensorId());
-        temperatureRepository.save(temperature);
-        System.out.println("Motion data saved: " + temperature);
-        System.out.println(temperature);
-    }
-}
+//package projectKDG.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import projectKDG.domain.Temperature;
+//import projectKDG.repository.TemperatureRepository;
+//
+//import java.util.List;
+//
+//@Service
+//public class TemperatureService{
+//    private final TemperatureRepository temperatureRepository;
+//
+//    @Autowired
+//    public TemperatureService(TemperatureRepository temperatureRepository) {
+//        this.temperatureRepository = temperatureRepository;
+//    }
+//
+//    @GetMapping
+//    public List<Temperature> getTemperatures() {
+//        return temperatureRepository.findAll();
+//    }
+//
+//    public void addNewTemperature(Temperature temperature) {
+//        temperatureRepository.findTemperatureByTemperatureSensorId(temperature.getTemperatureSensorId());
+//        temperatureRepository.save(temperature);
+//        System.out.println("Motion data saved: " + temperature);
+//        System.out.println(temperature);
+//    }
+//}
