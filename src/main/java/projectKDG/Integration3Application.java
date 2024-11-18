cc47b99f6118f1a1b8a4d8c926a77406e1e78e63
Viktória Dobišová <viktoria.dobisova@student.kdg.be>
2024-11-18 12:35:23 +0000

    Revert "Merge branch 'main' of https://gitlab.com/kdg-ti/integration-2.1/24-25/team-7/Kitchen-Danger_Guard"
    This reverts commit f83cbe7ef650a9000ff1c1940cf25d0d75e3b264

--- src/main/java/projectKDG/Integration3Application.java
+++ src/main/java/projectKDG/Integration3Application.java
@@ -2,10 +2,8 @@ package projectKDG;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "projectKDG")
 public class Integration3Application {
 
     public static void main(String[] args) {
